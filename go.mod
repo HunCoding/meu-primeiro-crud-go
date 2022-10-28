81e3cba5a952a3ccf3d8eb1924bb04f9106b6bcb
Otavio Celestino dos Santos <hunter@pop-os.localdomain>
2022-10-27 21:08:32 -0300

    Adicionando arquivos iniciais

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/HunCoding/meu-primeiro-crud-go
+
+go 1.19
+
+require github.com/joho/godotenv v1.4.0 // indirect
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	fmt.Println(os.Getenv("TEST"))
+}
