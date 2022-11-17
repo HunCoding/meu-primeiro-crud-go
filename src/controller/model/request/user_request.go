6e3e77b54a8552df846d1f0243a01a295c7cfff0
Otavio Celestino dos Santos <hunter@pop-os.localdomain>
2022-11-17 15:47:47 -0300

    Adicionando validação dos campos de requisição

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -1,8 +1,8 @@
 package request
 
 type UserRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
-	Age      int8   `json:"age"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6,containsany=!@#$%*"`
+	Name     string `json:"name" binding:"required,min=4,max=100"`
+	Age      int8   `json:"age" binding:"required,min=1,max=140"`
 }
