26308dc6944aa8985851c7439015477ddb4de252
Hunter <otavio201378@gmail.com>
2023-06-21 22:45:00 -0300

    Adicionando testes unitários para o create_user_repository.go

--- go.mod
+++ go.mod
@@ -7,16 +7,19 @@ require (
 	github.com/go-playground/locales v0.14.0
 	github.com/go-playground/universal-translator v0.18.0
 	github.com/go-playground/validator/v10 v10.11.1
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/joho/godotenv v1.4.0
+	github.com/stretchr/testify v1.8.0
 	go.mongodb.org/mongo-driver v1.11.1
 	go.uber.org/zap v1.24.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/goccy/go-json v0.9.11 // indirect
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/google/go-cmp v0.5.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
@@ -26,6 +29,7 @@ require (
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.1 // indirect
@@ -40,4 +44,5 @@ require (
 	golang.org/x/text v0.4.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ src/model/repository/create_user_repository_test.go
@@ -0,0 +1,64 @@
+package repository
+
+import (
+	"github.com/HunCoding/meu-primeiro-crud-go/src/model"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/integration/mtest"
+	"os"
+	"testing"
+)
+
+func TestUserRepository_CreateUser(t *testing.T) {
+	database_name := "user_database_test"
+	collection_name := "user_collection_test"
+
+	err := os.Setenv("MONGODB_USER_DB", collection_name)
+	if err != nil {
+		t.FailNow()
+		return
+	}
+	defer os.Clearenv()
+
+	mtestDb := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+	defer mtestDb.Close()
+
+	mtestDb.Run("when_sending_a_valid_domain_returns_success", func(mt *mtest.T) {
+		mt.AddMockResponses(bson.D{
+			{Key: "ok", Value: 1},
+			{Key: "n", Value: 1},
+			{Key: "acknowledged", Value: true},
+		})
+		databaseMock := mt.Client.Database(database_name)
+
+		repo := NewUserRepository(databaseMock)
+		domain := model.NewUserDomain(
+			"test@test.com", "test", "test", 90)
+		userDomain, err := repo.CreateUser(domain)
+
+		_, errId := primitive.ObjectIDFromHex(userDomain.GetID())
+
+		assert.Nil(t, err)
+		assert.Nil(t, errId)
+		assert.EqualValues(t, userDomain.GetEmail(), domain.GetEmail())
+		assert.EqualValues(t, userDomain.GetName(), domain.GetName())
+		assert.EqualValues(t, userDomain.GetAge(), domain.GetAge())
+		assert.EqualValues(t, userDomain.GetPassword(), domain.GetPassword())
+	})
+
+	mtestDb.Run("return_error_from_database", func(mt *mtest.T) {
+		mt.AddMockResponses(bson.D{
+			{Key: "ok", Value: 0},
+		})
+		databaseMock := mt.Client.Database(database_name)
+
+		repo := NewUserRepository(databaseMock)
+		domain := model.NewUserDomain(
+			"test@test.com", "test", "test", 90)
+		userDomain, err := repo.CreateUser(domain)
+
+		assert.NotNil(t, err)
+		assert.Nil(t, userDomain)
+	})
+}
