3405d4c8c674625e4ae5f9b5f5e1906f98619a98
Hunter <otavio201378@gmail.com>
2023-07-06 11:59:56 -0300

    Adicionando testes unitários para o findUserServices da aplicação

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/joho/godotenv v1.4.0
 	github.com/stretchr/testify v1.8.0
 	go.mongodb.org/mongo-driver v1.11.1
+	go.uber.org/mock v0.1.0
 	go.uber.org/zap v1.24.0
 )
 
---
+++ src/model/service/find_user_test.go
@@ -0,0 +1,128 @@
+package service
+
+import (
+	"github.com/HunCoding/meu-primeiro-crud-go/src/configuration/rest_err"
+	"github.com/HunCoding/meu-primeiro-crud-go/src/model"
+	"github.com/HunCoding/meu-primeiro-crud-go/src/tests/mocks"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.uber.org/mock/gomock"
+	"math/rand"
+	"strconv"
+	"testing"
+)
+
+func TestUserDomainService_FindUserByIDServices(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repository := mocks.NewMockUserRepository(ctrl)
+	service := NewUserDomainService(repository)
+
+	t.Run("when_exists_an_user_returns_success", func(t *testing.T) {
+		id := primitive.NewObjectID().Hex()
+
+		userDomain := model.NewUserDomain("test@test.com", "test", "test", 50)
+		userDomain.SetID(id)
+
+		repository.EXPECT().FindUserByID(id).Return(userDomain, nil)
+
+		userDomainReturn, err := service.FindUserByIDServices(id)
+
+		assert.Nil(t, err)
+		assert.EqualValues(t, userDomainReturn.GetID(), id)
+		assert.EqualValues(t, userDomainReturn.GetEmail(), userDomain.GetEmail())
+		assert.EqualValues(t, userDomainReturn.GetPassword(), userDomain.GetPassword())
+		assert.EqualValues(t, userDomainReturn.GetName(), userDomain.GetName())
+		assert.EqualValues(t, userDomainReturn.GetAge(), userDomain.GetAge())
+	})
+
+	t.Run("when_does_not_exists_an_user_returns_error", func(t *testing.T) {
+		id := primitive.NewObjectID().Hex()
+
+		repository.EXPECT().FindUserByID(id).Return(nil, rest_err.NewNotFoundError("user not found"))
+		userDomainReturn, err := service.FindUserByIDServices(id)
+
+		assert.Nil(t, userDomainReturn)
+		assert.NotNil(t, err)
+		assert.EqualValues(t, err.Message, "user not found")
+	})
+}
+
+func TestUserDomainService_FindUserByEmailServices(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repository := mocks.NewMockUserRepository(ctrl)
+	service := NewUserDomainService(repository)
+
+	t.Run("when_exists_an_user_returns_success", func(t *testing.T) {
+		id := primitive.NewObjectID().Hex()
+		email := "test@success.com"
+
+		userDomain := model.NewUserDomain(email, "test", "test", 50)
+		userDomain.SetID(id)
+
+		repository.EXPECT().FindUserByEmail(email).Return(userDomain, nil)
+
+		userDomainReturn, err := service.FindUserByEmailServices(email)
+
+		assert.Nil(t, err)
+		assert.EqualValues(t, userDomainReturn.GetID(), id)
+		assert.EqualValues(t, userDomainReturn.GetEmail(), userDomain.GetEmail())
+		assert.EqualValues(t, userDomainReturn.GetPassword(), userDomain.GetPassword())
+		assert.EqualValues(t, userDomainReturn.GetName(), userDomain.GetName())
+		assert.EqualValues(t, userDomainReturn.GetAge(), userDomain.GetAge())
+	})
+
+	t.Run("when_does_not_exists_an_user_returns_error", func(t *testing.T) {
+		email := "test@error.com"
+
+		repository.EXPECT().FindUserByEmail(email).Return(nil, rest_err.NewNotFoundError("user not found"))
+		userDomainReturn, err := service.FindUserByEmailServices(email)
+
+		assert.Nil(t, userDomainReturn)
+		assert.NotNil(t, err)
+		assert.EqualValues(t, err.Message, "user not found")
+	})
+}
+
+func TestUserDomainService_FindUserByEmailAndPasswordServices(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repository := mocks.NewMockUserRepository(ctrl)
+	service := &userDomainService{repository}
+
+	t.Run("when_exists_an_user_returns_success", func(t *testing.T) {
+		id := primitive.NewObjectID().Hex()
+		email := "test@success.com"
+		password := strconv.FormatInt(rand.Int63(), 10)
+
+		userDomain := model.NewUserDomain(email, password, "test", 50)
+		userDomain.SetID(id)
+
+		repository.EXPECT().FindUserByEmailAndPassword(email, password).Return(userDomain, nil)
+
+		userDomainReturn, err := service.findUserByEmailAndPasswordServices(email, password)
+
+		assert.Nil(t, err)
+		assert.EqualValues(t, userDomainReturn.GetID(), id)
+		assert.EqualValues(t, userDomainReturn.GetEmail(), userDomain.GetEmail())
+		assert.EqualValues(t, userDomainReturn.GetPassword(), userDomain.GetPassword())
+		assert.EqualValues(t, userDomainReturn.GetName(), userDomain.GetName())
+		assert.EqualValues(t, userDomainReturn.GetAge(), userDomain.GetAge())
+	})
+
+	t.Run("when_does_not_exists_an_user_returns_error", func(t *testing.T) {
+		email := "test@error.com"
+		password := strconv.FormatInt(rand.Int63(), 10)
+
+		repository.EXPECT().FindUserByEmailAndPassword(email, password).Return(nil, rest_err.NewNotFoundError("user not found"))
+		userDomainReturn, err := service.findUserByEmailAndPasswordServices(email, password)
+
+		assert.Nil(t, userDomainReturn)
+		assert.NotNil(t, err)
+		assert.EqualValues(t, err.Message, "user not found")
+	})
+}
